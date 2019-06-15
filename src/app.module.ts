aee722c77322f96530a3c214981295d15457ce21
chenreuven <chen.rvn@gmail.com>
2019-06-15 23:04:25 +0300

    add graphql capability

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,17 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CatsModule } from './cats/cats.module';
 import { EventsModule } from './events/events.module';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://username:password@cluster0-nxbix.mongodb.net/test?retryWrites=true&w=majority'),
-    CatsModule
+    //MongooseModule.forRoot('mongodb+srv://username:password@cluster0-nxbix.mongodb.net/test?retryWrites=true&w=majority\n'),
+    //CatsModule,
+    EventsModule,
+    GraphQLModule.forRoot({
+      debug: true,
+      playground: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
